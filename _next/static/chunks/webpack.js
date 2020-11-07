1c77b85bbaa81f5f3e33fd9456230c5e1ed04b99
Jakob Kraus <jakob.kraus@limebit.de>
2020-11-07 04:07:11 +0100

    Updated website

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "45875ff4040facf5496f";
+/******/ 	var hotCurrentHash = "f9e17e6246eac326f7d7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
