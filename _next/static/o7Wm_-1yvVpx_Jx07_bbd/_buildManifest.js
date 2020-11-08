b9bcb8532018f641425a0f62e7dc700050e6bf7b
JabobKrauskopf <JabobKrauskopf@users.noreply.github.com>
2020-11-08 16:31:59 +0000

    deploy: b95f6d122063eb880df2c2e19a47e25329cc220a

---
+++ _next/static/o7Wm_-1yvVpx_Jx07_bbd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1bfc9850.783dd75c5cfadddae5b5.js","static\u002Fchunks\u002Fpages\u002Findex-6f9c39eee0554f44b291.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fb942535921b5be64a37.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
